29bb361141a628435e351fd29072fd71d7000b5d
Yuriy Borys <iam618384@gmail.com>
2024-07-08 19:50:39 +0300

    Updated task 3

--- task-3.py
+++ task-3.py
@@ -1,9 +1,13 @@
 import re
 
 
+num_len = 9
+
+
 def normalize_phone(phone_number: str) -> str:
-    trimmed = ''.join(re.findall(r"\+?\d+", phone_number))
-    if not re.search(r"\+", trimmed):
-        trimmed = f"+380{trimmed[len(trimmed) - 9:]}"
+    trimmed = ''.join(re.findall(r"\+380|\d+", phone_number))
+
+    if not re.search(r"\+380", trimmed):
+        trimmed = f"+380{trimmed[len(trimmed) - num_len:]}"
 
     return trimmed
---
+++ task-4.py
@@ -0,0 +1,15 @@
+users = [
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"},
+    {"name": "Alex Longhorn", "birthday": "1982.02.02"},
+    {"name": "Barbara Jackson", "birthday": "1995.06.15"},
+    {"name": "Anthony McBright", "birthday": "1990.01.27"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+
+
+def get_upcoming_birthdays():
+    pass
+
